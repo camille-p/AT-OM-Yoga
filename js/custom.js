109ecd2843f1a41b81ec8faf67c67134b804bb9f
Camille <puyocamille@gmail.com>
2018-05-08 12:32:24 +0100

    changed the colours of the website, put the photos black and white, changed the photo of the top page and made changes accordingly.

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,16 @@ $(document).ready(function(){
         $("nav ul").toggleClass("showing");
     });
 });
+
+
+
+
+
+
+
+
+
+$(".Sign").on("click", function(event){
+    $("#signUp").slideToggle();
+    event.preventDefault();
+});
