a6b6a9f521c786e80ed567afa24b86e19d990d57
Camille <puyocamille@gmail.com>
2018-05-14 13:09:52 +0100

    added an option to sign up to the newsletter with a personalised message when submitting

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+//Hamburger menu
+
 $(document).ready(function(){
     $(".menu-icon").on("click", function(){
         $("nav ul").toggleClass("showing");
@@ -5,13 +7,29 @@ $(document).ready(function(){
 });
 
 
+//To Sign Up for Classes
+
 $(".Sign").on("click", function(){
-$("#signUp, .modal-box").addClass("active");
+  $("#signUp, .modal-box").addClass("active");
 });
 
 $(".close").on("click", function(){
-$("#signUp, .modal-box").removeClass("active");
+  $("#signUp, .modal-box").removeClass("active");
 });
 
 
-    
+
+//To Sign Up for the Newsletter
+
+$(".Newsletter").on("click", function(){
+  $("#signNewsletter, .sign-newsletter").addClass("active");
+});
+
+$(".close").on("click", function(){
+ $("#signNewsletter, .sign-newsletter").removeClass("active");
+});
+
+$("form#signNewsletter").on("submit", function() {
+   var first_name = $("input#firstName").val();
+    $("h2#playback").text("Thank You " + first_name + " ");
+});
